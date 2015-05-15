556a441461db8fff6b7890b876b5a08601fd5549
imperiumzigna <igoramorimsilva2@gmail.com>
2015-05-14 23:10:48 -0300

    marcas editada

--- build-src-Desktop_Qt_5_4_1_MSVC2013_OpenGL_64bit-Debug/ui_wizardmarcas.h
+++ build-src-Desktop_Qt_5_4_1_MSVC2013_OpenGL_64bit-Debug/ui_wizardmarcas.h
@@ -14,19 +14,55 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QDialog>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QScrollBar>
+#include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
 
 class Ui_wizardMarcas
 {
 public:
+    QScrollBar *verticalScrollBar;
+    QFrame *frame;
+    QWidget *gridLayoutWidget;
+    QGridLayout *gridLayout;
 
     void setupUi(QDialog *wizardMarcas)
     {
         if (wizardMarcas->objectName().isEmpty())
             wizardMarcas->setObjectName(QStringLiteral("wizardMarcas"));
-        wizardMarcas->resize(576, 433);
+        wizardMarcas->resize(714, 479);
+        wizardMarcas->setMinimumSize(QSize(0, 0));
+        wizardMarcas->setStyleSheet(QLatin1String("#wizardMarcas{\n"
+"background-color:rgb(26,26,26);\n"
+"\n"
+"}"));
+        verticalScrollBar = new QScrollBar(wizardMarcas);
+        verticalScrollBar->setObjectName(QStringLiteral("verticalScrollBar"));
+        verticalScrollBar->setGeometry(QRect(500, 0, 16, 481));
+        verticalScrollBar->setStyleSheet(QStringLiteral(""));
+        verticalScrollBar->setOrientation(Qt::Vertical);
+        frame = new QFrame(wizardMarcas);
+        frame->setObjectName(QStringLiteral("frame"));
+        frame->setGeometry(QRect(510, 0, 211, 481));
+        frame->setStyleSheet(QLatin1String("#frame{\n"
+"background-color:gray;\n"
+"\n"
+"}"));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+        gridLayoutWidget = new QWidget(wizardMarcas);
+        gridLayoutWidget->setObjectName(QStringLiteral("gridLayoutWidget"));
+        gridLayoutWidget->setGeometry(QRect(20, 20, 471, 451));
+        gridLayout = new QGridLayout(gridLayoutWidget);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        frame->raise();
+        verticalScrollBar->raise();
+        gridLayoutWidget->raise();
 
         retranslateUi(wizardMarcas);
 
