bb962b6c8ad86368b3f8e24ef3904d54090dfe01
imperiumzigna <igoramorimsilva2@gmail.com>
2015-05-15 19:37:59 -0300

    nada relevante

--- src/wizardmarcas.cpp
+++ src/wizardmarcas.cpp
@@ -1,11 +1,22 @@
 #include "wizardmarcas.h"
 #include "ui_wizardmarcas.h"
+#include <QPushButton>
 
+//int i=0;
 wizardMarcas::wizardMarcas(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::wizardMarcas)
 {
     ui->setupUi(this);
+    /* Colocar a conexão entre a base de dados com as imagens das marcas no grid layout
+     * em casa grid colocar um botão com um ícone vindo da base
+     * QPushButton button;
+     * while(i<=número de imagens){
+     * alguma coisa
+     *
+     * i=i+1;
+     * }
+     * */
 }
 
 wizardMarcas::~wizardMarcas()
